797a411d5e879f35ea3c50c761f1c67a1acabaab
ksang123 <etaizilberman@gmail.com>
2025-07-04 03:23:24 +0300

    moved the flag

--- hypothesis-python/src/hypothesis/core.py
+++ hypothesis-python/src/hypothesis/core.py
@@ -1238,8 +1238,7 @@ class StateForActualGivenExecution:
 
     #TODO: we need to improve this
     def save_failing_explicit_example(
-        self, example_kwargs: dict[str, object], origin: InterestingOrigin, copy_code: bool = True
-    ) -> None:
+        self, example_kwargs: dict[str, object], origin: InterestingOrigin) -> None:
         """Record failing explicit examples for UnitTestGenerator."""
         import os.path
 
@@ -1340,7 +1339,8 @@ class StateForActualGivenExecution:
                 "filename": source_filename,
                 "lineno": line_number,
                 "test_func": self.test,  # Pass the actual function for source extraction
-                "copy_code": copy_code,  # Pass the flag to the generator
+                "given_kwargs": {},
+                "choices": [],
             }, output_file
         )
 
